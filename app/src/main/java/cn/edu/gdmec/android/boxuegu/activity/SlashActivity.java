75b59a9d1cea01b93bbe208f45094cd4cfe86c93
Ton-Cai <1583153972@qq.com>
2017-12-26 14:32:54 +0800

    new change

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SlashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SlashActivity.java
@@ -1,13 +1,15 @@
-package cn.edu.gdmec.android.boxuegu;
+package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cn.edu.gdmec.android.boxuegu.R;
+
+public class SlashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_slash);
     }
 }
