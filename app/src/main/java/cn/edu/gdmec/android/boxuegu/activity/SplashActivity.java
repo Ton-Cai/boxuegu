769cb44aed166da5830b1a68d754ba3688d56b80
Ton-Cai <1583153972@qq.com>
2017-12-27 08:49:54 +0800

    new change

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slash);
+        setContentView(R.layout.activity_splash);
         //设置此界面为竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         init();
