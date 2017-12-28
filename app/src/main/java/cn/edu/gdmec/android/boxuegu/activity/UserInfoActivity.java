f05326f66b86e15cf91efbcc02ff116f4b05054a
HeJie47 <896419504@qq.com>
2017-12-28 10:22:10 +0800

    第3章

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
@@ -9,7 +9,9 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.bean.UserBean;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
+import cn.edu.gdmec.android.boxuegu.utils.DBUtils;
 
 public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tv_back;
@@ -19,6 +21,8 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
     private static final int CHANGE_NICKNAME = 1;//修改昵称的自定义常量
     private static final int CHANGE_SIGNATURE = 2; //修改个性签名的自定义常量
     private String spUserName;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +48,28 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
         rl_sex = (RelativeLayout) findViewById(R.id.rl_sex);
         rl_signature = (RelativeLayout) findViewById(R.id.rl_signature);
         tv_nickName = (TextView) findViewById(R.id.tv_nickName);
-        tv_nickName = (TextView) findViewById(R.id.tv_nickName);
-        tv_nickName = (TextView) findViewById(R.id.tv_nickName);
+        tv_user_name = (TextView) findViewById(R.id.tv_user_name);
+        tv_sex = (TextView) findViewById(R.id.tv_sex);
+        tv_signature = (TextView) findViewById(R.id.tv_signature);
     }
 
+    /**
+     * 获取数据
+     */
     private void initData() {
-
+//        UserBean bean = unll;
+//        bean = DBUtils.getInstance(this).getUserInfo(spUserName);
+//        //首先判断一下数据库是否有数据
+//        if (bean == null){
+//            bean = new UserName();
+//            bean.userName = spUserName;
+//            bean.nickName = "问答精灵";
+//            bean.sex = "男";
+//            bean.signature = "问答精灵";
+//            //保存用户信息到数据库
+//            DBUtils.getInstance(this).savedUserInfo(bean);
+//        }
+//        setValue(bean);
     }
 
     private void setListener() {
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/UserBean.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.boxuegu.bean;
+
+/**
+ * Created by student on 17/12/28.
+ */
+
+public class UserBean {
+    //用户名
+    public String userName;
+    //昵称
+    public String nickName;
+    //性别
+    public String sex;
+    //签名
+    public String signature;
+}
