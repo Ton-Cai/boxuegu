306b35f410efc08fba5bdb383a2495ec00f3e01f
HeJie47 <896419504@qq.com>
2017-12-28 11:16:51 +0800

    更正1

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/FindPswActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/FindPswActivity.java
@@ -1,6 +1,7 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -16,20 +17,22 @@ import cn.edu.gdmec.android.boxuegu.utils.MD5Utils;
 
 public class FindPswActivity extends AppCompatActivity {
 
+    private String from;
     private TextView tv_main_title;
     private TextView tv_back;
     private EditText et_validate_name;
     private Button btn_validate;
     private TextView tv_reset_psw;
-    private EditText et_user_name;
+    private TextView et_user_name;
     private TextView tv_user_name;
-    private String from;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_psw);
-        //获取从登陆界面和设置界面传递的数据
+        //设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //获取从登录界面和设置界面传递的数据
         from = getIntent().getStringExtra("from");
         init();
     }
@@ -37,55 +40,61 @@ public class FindPswActivity extends AppCompatActivity {
     private void init() {
         tv_main_title = (TextView) findViewById(R.id.tv_main_title);
         tv_back = (TextView) findViewById(R.id.tv_back);
-
         et_validate_name = (EditText) findViewById(R.id.et_validate_name);
         btn_validate = (Button) findViewById(R.id.btn_validate);
         tv_reset_psw = (TextView) findViewById(R.id.tv_reset_psw);
-        et_user_name = (EditText) findViewById(R.id.et_user_name);
+        et_user_name = (TextView) findViewById(R.id.et_user_name);
         tv_user_name = (TextView) findViewById(R.id.tv_user_name);
-        if ("security".equals(from)){
+        if ("security".equals(from)) {
             tv_main_title.setText("设置密保");
-        }else {
+        } else{
             tv_main_title.setText("找回密码");
             tv_user_name.setVisibility(View.VISIBLE);
             et_user_name.setVisibility(View.VISIBLE);
         }
 
+        tv_back.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FindPswActivity.this.finish();
+            }
+        });
+
         btn_validate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String validateName = et_validate_name.getText().toString().trim();
-                if ("security".equals("from")){
-                    if (TextUtils.isEmpty(validateName)){//设置密保
-                        Toast.makeText(FindPswActivity.this, "请输入要验证的姓名", Toast.LENGTH_LONG).show();
+                if ("security".equals(from)) {//设置密保
+                    if (TextUtils.isEmpty(validateName)) {
+                        Toast.makeText(FindPswActivity.this, "请输入要验证的姓名", Toast.LENGTH_SHORT).show();
                         return;
-                    }else{
-                        Toast.makeText(FindPswActivity.this, "请输入要验证的姓名", Toast.LENGTH_LONG).show();
-                        //保存密保到sharedpreference中
-                        savaSecurity(validateName);
-                        finish();
+                    } else {
+                        Toast.makeText(FindPswActivity.this, "设置密保成功", Toast.LENGTH_SHORT).show();
+                        //保存密保到Sharedpreference中
+                        saveSecurity(validateName);
+                        FindPswActivity.this.finish();
                     }
-                }else{
+                } else {
                     //找回密码
                     String userName = et_user_name.getText().toString().trim();
                     String sp_security = readSecurity(userName);
-
-                    if (TextUtils.isEmpty(userName)){
+                    if (TextUtils.isEmpty(userName)) {
                         Toast.makeText(FindPswActivity.this, "请输入您的用户名", Toast.LENGTH_SHORT).show();
                         return;
-                    }else if (!isExistUserName(userName)){
-                        Toast.makeText(FindPswActivity.this, "您输入的用户名不存在", Toast.LENGTH_SHORT).show();
+                    } else if (!isExistUserName(userName)) {
+                        Toast.makeText(FindPswActivity.this, "您输入的用户名是不存在的", Toast.LENGTH_SHORT).show();
                         return;
-                    }else if (TextUtils.isEmpty(validateName)){
+                    } else if (TextUtils.isEmpty(validateName)) {
                         Toast.makeText(FindPswActivity.this, "请输入要验证的姓名", Toast.LENGTH_SHORT).show();
                         return;
-                    }else if (!validateName.equals(sp_security)){
+                    } else if (!validateName.equals(sp_security)) {
                         Toast.makeText(FindPswActivity.this, "输入的密保不正确", Toast.LENGTH_SHORT).show();
                         return;
-                    }else{
-                        //用户输入的密保证正确，重新给用户设置一个密码
+                    } else {
+                        //用户输入的密保正确，重新给用户设置一个密码
                         tv_reset_psw.setVisibility(View.VISIBLE);
-                        tv_reset_psw.setText("初始密码：123456");
+                        tv_reset_psw.setText("初始密码: 123456");
                         savePsw(userName);
                     }
                 }
@@ -108,8 +117,8 @@ public class FindPswActivity extends AppCompatActivity {
     private boolean isExistUserName(String userName) {
         boolean hasUserName = false;
         SharedPreferences sp = getSharedPreferences("loginInfo", MODE_PRIVATE);
-        String spPsw = sp.getString("userName", "");
-        if (!TextUtils.isEmpty(spPsw)){
+        String spPsw = sp.getString(userName, "");
+        if (!TextUtils.isEmpty(spPsw)) {
             hasUserName = true;
         }
         return hasUserName;
@@ -121,10 +130,13 @@ public class FindPswActivity extends AppCompatActivity {
         return security;
     }
 
-    private void savaSecurity(String validateName) {
+    private void saveSecurity(String validateName) {
         SharedPreferences sp = getSharedPreferences("loginInfo", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString(AnalysisUtils.readLoginUserName(this) + "_security", validateName); //存入用户对应的密保
+        editor.putString(AnalysisUtils.readLoginUserName(this) + "_security", validateName);//存入用户对应的密保
         editor.commit();
+
+
     }
 }
+
